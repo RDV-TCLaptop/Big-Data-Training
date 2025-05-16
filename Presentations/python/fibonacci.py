cd0a8f8c9ab6f4f2e0fc11ddac63008ff443e7dd
rdv.tclaptop@gmail.com <rdv.tclaptop@gmail.com>
2025-05-16 08:50:26 -0400

    updates

---
+++ Presentations/python/fibonacci.py
@@ -0,0 +1,17 @@
+# Example for module
+def fibonacci_write(n):
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a + b
+
+def fibonacci_return(n):
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a + b
+    return result
+
+if __name__ == "__main__":
+    fibonacci_write(1000)
